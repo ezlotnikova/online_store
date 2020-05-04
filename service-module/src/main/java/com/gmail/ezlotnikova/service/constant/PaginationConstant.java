e7b7f33d0fd1962688369a68532a3d55c50d5d14
Katsiaryna Zlotnikava <e.zlotnikova@gmail.com>
2020-05-04 13:19:18 +0300

    Very Last Corrections

--- service-module/src/main/java/com/gmail/ezlotnikova/service/constant/PaginationConstant.java
+++ service-module/src/main/java/com/gmail/ezlotnikova/service/constant/PaginationConstant.java
@@ -2,10 +2,10 @@ package com.gmail.ezlotnikova.service.constant;
 
 public interface PaginationConstant {
 
-    int REVIEWS_BY_PAGE = 3;
-    int USERS_BY_PAGE = 3;
-    int ARTICLES_BY_PAGE = 3;
-    int ITEMS_BY_PAGE = 3;
-    int ORDERS_BY_PAGE = 3;
+    int REVIEWS_BY_PAGE = 10;
+    int USERS_BY_PAGE = 10;
+    int ARTICLES_BY_PAGE = 10;
+    int ITEMS_BY_PAGE = 10;
+    int ORDERS_BY_PAGE = 10;
 
 }
